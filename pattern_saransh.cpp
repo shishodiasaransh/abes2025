ae5dd8c0f56c4a67ad19aef9af8bc772f9dcb38c
shishodiasaransh <109784419+shishodiasaransh@users.noreply.github.com>
2024-12-20 14:45:18 +0530

    pattern_saransh.cpp

--- pattern_saransh.cpp
+++ pattern_saransh.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 int main() {
     int n=5;
-    cout << "Enter the number of rows: ";
-    cin >> n;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n - i; j++) {
             cout << " ";
